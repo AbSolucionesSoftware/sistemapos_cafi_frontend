5c5271166aa3f8a3b2ea592900fe419800e51363
Diego <consultoresartificialbrain@gmail.com>
2021-06-30 09:59:51 -0500

    agregando cambios almacen

--- src/pages/sucursales/Catalogos/Departamentos/RegistroDepartamento.js
+++ src/pages/sucursales/Catalogos/Departamentos/RegistroDepartamento.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, TextField } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import TablaDepartamentos from './ListaDepartamentos';
+import { OBTENER_DEPARTAMENTOS, REGISTRAR_DEPARTAMENTO, ACTUALIZAR_DEPARTAMENTO } from '../../../../gql/Catalogos/departamentos';
+import { useMutation, useQuery } from '@apollo/client';
+import { CreateDepartamentosContext } from '../../../../context/Catalogos/createDepartamentos'
+
 
 export default function RegistroDepartamentos() {
+
+	const [data, setdata] = useState({});
+
+	const obtenerDatos = (e) => {
+		setdata({
+			...data,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const [ CrearDepartamentos ] = useMutation(REGISTRAR_DEPARTAMENTO);
+
+	const saveData = () => {
+		try {
+			
+		} catch (error) {
+			
+		}
+	}
+
 	return (
 		<Box>
 			<Box display="flex" justifyContent="center" alignItems="center" my={2}>
 				<TextField
-					/* error */
 					id="outlined-error-helper-text"
 					label="Nombre departamento"
-					defaultValue="Departament de embutidos"
-					/* helperText="Incorrect entry." */
+					value={data.nombre_departamento}
+					name="nombre_departamento"
 					variant="outlined"
 					size="small"
                     fullWidth
+					onChange={obtenerDatos}
 				/>
 				<Box ml={1} />
 				<Button color="primary" variant="contained" size="large" disableElevation>
