95db08ec341db2c30411d9d35bceca367ddee651
Diego <consultoresartificialbrain@gmail.com>
2021-08-25 14:40:34 -0500

    Agregando tallas

--- src/pages/sucursales/Catalogos/Producto/TallasColores/TallasColores.js
+++ src/pages/sucursales/Catalogos/Producto/TallasColores/TallasColores.js
@@ -49,13 +49,137 @@ export default function ColoresTallas({ obtenerConsultasProducto, refetch, datos
 	};
 
 	const handleAddTallas = (event, child) => {
+		console.log(child.props.children[0].props.checked);
 		const medidas_seleccionadas_temp = event.target.value;
 		let presentacion = [];
+		const array_medidad_finales = presentaciones;
 
-		if (!coloresSeleccionados.length) {
-			/* SI NO HAY COLORES SE AGREGAN SOLO TALLLAS */
+		if(!coloresSeleccionados.length && !array_medidad_finales.length ){
+			console.log("Primera talla agregada");
+			for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
+				const producto_medida = medidas_seleccionadas_temp[i];
+				let producto = {
+					existencia: false,
+					codigo_barras: datos_generales.codigo_barras,
+					nombre_comercial: datos_generales.nombre_comercial,
+					medida: producto_medida,
+					color: { nombre: '', hex: '' },
+					precio: preciosP[0].precio_neto,
+					cantidad: 0
+				};
+				presentacion.push(producto);
+			}
+		}else if(!coloresSeleccionados.length && array_medidad_finales.length > 0){
+			console.log("Ya existe tallas se agregan mas");
+			for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
+				const producto_medida = medidas_seleccionadas_temp[i];
+				const result = array_medidad_finales.filter((res) => res.medida._id === producto_medida._id);
+				if (result.length) {
+					presentacion.push(result[0]);
+				} else {
+					let producto = {
+						existencia: false,
+						codigo_barras: datos_generales.codigo_barras,
+						nombre_comercial: datos_generales.nombre_comercial,
+						medida: producto_medida,
+						color: { nombre: '', hex: '' },
+						precio: preciosP[0].precio_neto,
+						cantidad: 0
+					};
+					presentacion.push(producto);
+				}
+			}
+		}else if(coloresSeleccionados.length > 0 && medidas_seleccionadas_temp.length === 1 && child.props.children[0].props.checked === false){
+			console.log("Hay colores y se agrega talla por primera vez");
+			for (let i = 0; i < array_medidad_finales.length; i++) {
+				for (let k = 0; k < medidas_seleccionadas_temp.length; k++) {
+					presentacion.push({
+						existencia: array_medidad_finales[i].existencia,
+						codigo_barras: array_medidad_finales[i].codigo_barras,
+						nombre_comercial: array_medidad_finales[i].nombre_comercial,
+						medida: medidas_seleccionadas_temp[k],
+						color: array_medidad_finales[i].color,
+						precio: array_medidad_finales[i].precio,
+						cantidad: array_medidad_finales[i].cantidad
+					});
+				}
+			}
+		}else if(coloresSeleccionados.length > 0 && medidas_seleccionadas_temp.length > 0){
+			console.log("Hay colores y medidas");
+			for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
+				const producto_medida = medidas_seleccionadas_temp[i];
+				for (let k = 0; k < coloresSeleccionados.length; k++) {
+					const producto_color = coloresSeleccionados[k];
+					const presentacion_existente = array_medidad_finales.filter(
+						(producto_array_final) =>
+						producto_array_final.medida._id === producto_medida._id && producto_color._id === producto_array_final.color._id
+					);
+					if (!presentacion_existente.length) {
+							presentacion.push({
+								existencia: false,
+								codigo_barras: datos_generales.codigo_barras,
+								nombre_comercial: datos_generales.nombre_comercial,
+								medida: producto_medida,
+								color: producto_color,
+								precio: preciosP[0].precio_neto,
+								cantidad: 0
+							});
+						/* const presentacion_color_existente = array_medidad_finales.filter(
+							(producto_array_final) =>
+							producto_color._id === producto_array_final.color._id
+						);
+						if(presentacion_color_existente.length > 0){
+							console.log("Existe el color: ",presentacion_color_existente[0].color.nombre," Cantidad: ",presentacion_color_existente[0].cantidad);
+							presentacion.push({
+								existencia: presentacion_color_existente[0].existencia,
+								codigo_barras: presentacion_color_existente[0].codigo_barras,
+								nombre_comercial: presentacion_color_existente[0].nombre_comercial,
+								medida: producto_medida,
+								color: presentacion_color_existente[0].color,
+								precio: presentacion_color_existente[0].precio,
+								cantidad: presentacion_color_existente[0].cantidad
+							});
+						}else{
+							presentacion.push({
+								existencia: false,
+								codigo_barras: datos_generales.codigo_barras,
+								nombre_comercial: datos_generales.nombre_comercial,
+								medida: producto_medida,
+								color: producto_color,
+								precio: preciosP[0].precio_neto,
+								cantidad: 0
+							});
+						} */
+					} else {
+						// console.log("Esta talla no se mueve");
+						presentacion.push(presentacion_existente[0]);
+					}
+				}
+			}
+		}else if(coloresSeleccionados.length > 0 && !medidas_seleccionadas_temp.length){
+			console.log("Cuando hay color, pero no hay talla");
+			/* si no hay tallas se vulven a listar las presentaciones originales si tenian colores */
+			const presentaciones_existentes = array_medidad_finales.filter((producto) => producto.medida._id);
+			if (presentaciones_existentes.length) {
+				for (let x = 0; x < array_medidad_finales.length; x++) {
+					const objeto_presentaciones_final = array_medidad_finales[x];
+					presentacion.push({
+						existencia: objeto_presentaciones_final.existencia,
+						codigo_barras: objeto_presentaciones_final.codigo_barras,
+						nombre_comercial: objeto_presentaciones_final.nombre_comercial,
+						medida: {},
+						color: objeto_presentaciones_final.color,
+						precio: objeto_presentaciones_final.precio,
+						cantidad: objeto_presentaciones_final.cantidad
+					});
+				}
+			}
+		}
+
+		/* if (!coloresSeleccionados.length) {
+			// SI NO HAY COLORES SE AGREGAN SOLO TALLLAS
 			if (!presentaciones.length) {
-				/* SI NO HAY PRESENTACIONES AGREGAR LA PRIMERA */
+				// SI NO HAY PRESENTACIONES AGREGAR LA PRIMERA
 				for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
 					const producto_medida = medidas_seleccionadas_temp[i];
 					let producto = {
@@ -70,7 +194,7 @@ export default function ColoresTallas({ obtenerConsultasProducto, refetch, datos
 					presentacion.push(producto);
 				}
 			} else {
-				/* SI SI HAY PRESENTACIONES.. DUPLICAR */
+				// SI SI HAY PRESENTACIONES.. DUPLICAR
 				for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
 					const producto_medida = medidas_seleccionadas_temp[i];
 					const result = presentaciones.filter((res) => res.medida._id === producto_medida._id);
@@ -91,7 +215,7 @@ export default function ColoresTallas({ obtenerConsultasProducto, refetch, datos
 				}
 			}
 		} else {
-			/* SI HAY COLORES SE AGREGARA LA TALLA A LOS COLORES EXISTENTES SI AGREGA MAS DE UN COLOR SE DEBE MULTIPLICAR ESA PRESENTACION */
+			// SI HAY COLORES SE AGREGARA LA TALLA A LOS COLORES EXISTENTES SI AGREGA MAS DE UN COLOR SE DEBE MULTIPLICAR ESA PRESENTACION
 			if (medidas_seleccionadas_temp.length) {
 				for (let i = 0; i < medidas_seleccionadas_temp.length; i++) {
 					const producto_medida = medidas_seleccionadas_temp[i];
@@ -119,7 +243,7 @@ export default function ColoresTallas({ obtenerConsultasProducto, refetch, datos
 					}
 				}
 			} else {
-				/* si no hay tallas se vulven a listar las presentaciones originales si tenian colores */
+				// si no hay tallas se vulven a listar las presentaciones originales si tenian colores
 				const presentaciones_existentes = presentaciones.filter((producto) => producto.medida._id);
 				if (presentaciones_existentes.length) {
 					for (let x = 0; x < presentaciones.length; x++) {
@@ -136,7 +260,7 @@ export default function ColoresTallas({ obtenerConsultasProducto, refetch, datos
 					}
 				}
 			}
-		}
+		} */
 
 		setMedidasSeleccionadas(medidas_seleccionadas_temp);
 		setPresentaciones(presentacion);
