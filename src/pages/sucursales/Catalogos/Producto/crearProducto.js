b20490b938b5bf4c09a5308b5b2488b8b9bfee34
Diego <consultoresartificialbrain@gmail.com>
2021-08-27 14:29:44 -0500

    agregando filtros antes de enviar la infor de edita

--- src/pages/sucursales/Catalogos/Producto/crearProducto.js
+++ src/pages/sucursales/Catalogos/Producto/crearProducto.js
@@ -226,14 +226,16 @@ export default function CrearProducto({ accion, datos, productosRefetch }) {
 			imagenes_without_aws = imagenes.filter((res) => !res.key_imagen);
 		}
 
+		
+
 		let input = {
-			datos_generales,
+			datos_generales: await validateJsonEdit(datos_generales,"datos_generales"),
 			precios,
 			imagenes: imagenes_without_aws,
 			imagenes_eliminadas,
 			almacen_inicial,
 			centro_de_costos,
-			unidades_de_venta: unidadesVenta,
+			unidades_de_venta: await validateJsonEdit(unidadesVenta,"unidades_de_venta"),
 			presentaciones,
 			precio_plazos: preciosPlazos,
 			empresa: sesion.empresa._id,
@@ -269,6 +271,45 @@ export default function CrearProducto({ accion, datos, productosRefetch }) {
 			setLoading(false);
 		}
 	};
+
+	const validateJsonEdit = async (data, tipo) => {
+		if(tipo === 'datos_generales'){
+			let object_date = {
+				clave_alterna: data.clave_alterna,
+				tipo_producto: data.tipo_producto,
+				nombre_comercial: data.nombre_comercial,
+				nombre_generico: data.nombre_generico,
+				receta_farmacia: data.receta_farmacia
+			}
+			if(data.codigo_barras !== null && data.codigo_barras !== '') object_date = {...object_date, codigo_barras: data.codigo_barras}
+			if(data.descripcion !== null && data.descripcion !== '') object_date = {...object_date, descripcion: data.descripcion}
+			if(data.id_categoria !== null && data.id_categoria !== '') object_date = {...object_date, id_categoria: data.id_categoria}
+			if(data.categoria !== null && data.categoria !== '') object_date = {...object_date, categoria: data.categoria}
+			if(data.subcategoria !== null && data.subcategoria !== '') object_date = {...object_date, subcategoria: data.subcategoria}
+			if(data.id_subcategoria !== null && data.id_subcategoria !== '') object_date = {...object_date, id_subcategoria: data.id_subcategoria}
+			if(data.id_departamento !== null && data.id_departamento !== '') object_date = {...object_date, id_departamento: data.id_departamento}
+			if(data.departamento !== null && data.departamento !== '') object_date = {...object_date, departamento: data.departamento}
+			if(data.id_marca !== null && data.id_marca !== '') object_date = {...object_date, id_marca: data.id_marca}
+			if(data.marca !== null && data.marca !== '') object_date = {...object_date, marca: data.marca}
+			if(data.clave_producto_sat !== null && data.clave_producto_sat !== '') object_date = {...object_date, clave_producto_sat: data.clave_producto_sat}
+			return object_date;
+		}else if(tipo === 'unidades_de_venta'){
+			let end_array = [];
+			for(var i=0; i < data.length; i++){
+				let object = {
+					_id: data[i]._id,
+					cantidad: data[i].cantidad,
+					id_producto: data[i].id_producto,
+					precio: data[i].precio,
+					unidad_principal: data[i].unidad_principal
+				};
+				if(data[i].codigo_barras !== null && data[i].codigo_barras !== '') object = {...object, codigo_barras: data[i].codigo_barras}
+				if(data[i].default !== null && data[i].default !== '') object = {...object, default: data[i].default}
+				end_array.push(object);
+			}
+			return end_array;
+		}
+	}
 
 	/* ###### RESET STATES ###### */
 	const resetInitialStates = () => {
