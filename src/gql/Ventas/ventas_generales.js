ad36b67da70f7a69f728ea3d285f562ae532d9d6
poday98 <poday736.98@gmail.com>
2021-12-07 15:07:04 -0600

    Busqueda productos ventas

--- src/gql/Ventas/ventas_generales.js
+++ src/gql/Ventas/ventas_generales.js
@@ -113,6 +113,76 @@ export const CONSULTA_PRODUCTO_UNITARIO = gql`
   }
 `;
 
+export const CONSULTA_PRODUCTOS_VENTAS = gql`
+  query ObtenerProductosVentas($empresa: ID!$sucursal: ID! $input: ObtenerProductosVentasInput) {
+    obtenerProductosVentas(empresa: $empresa sucursal: $sucursal input: $input) {
+      _id
+      precio
+      cantidad
+      concepto
+      unidad
+      unidad_principal
+      codigo_barras
+      descuento {
+        porciento
+        dinero_descontado
+        precio_con_descuento
+      }
+      descuento_activo
+      default
+      id_producto {
+        imagenes{
+          url_imagen
+        }
+        _id
+        datos_generales {
+          nombre_comercial
+          codigo_barras
+          tipo_producto
+          clave_alterna
+        }
+        precios {
+          ieps
+          ieps_activo
+          inventario {
+            inventario_minimo
+            inventario_maximo
+            unidad_de_inventario
+          }
+          iva
+          iva_activo
+          monedero
+          monedero_electronico
+          precio_de_compra {
+            precio_con_impuesto
+            precio_sin_impuesto
+            iva
+            ieps
+          }
+          precios_producto {
+            numero_precio
+            precio_neto
+            unidad_mayoreo
+            utilidad
+          }
+          unidad_de_compra {
+            cantidad
+            precio_unitario_con_impuesto
+            precio_unitario_sin_impuesto
+            unidad
+          }
+        }
+      }
+      inventario_general {
+        cantidad_existente
+        unidad_inventario
+        cantidad_existente_maxima
+        unidad_maxima
+      }
+    }
+  }
+`;
+
 export const OBTENER_CLIENTES_VENTAS = gql`
   query ObtenerClientesVentas($empresa: ID!, $sucursal: ID!) {
     obtenerClientesVentas(empresa: $empresa, sucursal: $sucursal) {
